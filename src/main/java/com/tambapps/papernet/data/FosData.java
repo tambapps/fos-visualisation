7dafbe25feed4d12f787b8e4154eaa452e88df73
nelson888 <tambue@hotmail.fr>
2020-02-09 20:58:12 +0100

    new function

--- src/main/java/com/tambapps/papernet/data/FosData.java
+++ src/main/java/com/tambapps/papernet/data/FosData.java
@@ -2,6 +2,10 @@ package com.tambapps.papernet.data;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Data
@@ -11,4 +15,16 @@ public class FosData {
   private final Map<Integer, Fos> fosByYear;
   private final Map<String, Integer> fosOccurenceMap;
 
+  public Map<String, List<Float>> getAllFosWeights(Collection<Fos> foses) {
+    Map<String, List<Float>> fosWeights = new HashMap<>();
+    foses.stream()
+      .map(Fos::getFosWeights)
+      .flatMap(map -> map.entrySet().stream())
+      .forEach(entry -> {
+        List<Float> weights = fosWeights.computeIfAbsent(entry.getKey(),
+          k -> new ArrayList<>());
+        weights.add(entry.getValue());
+      });
+    return fosWeights;
+  }
 }
