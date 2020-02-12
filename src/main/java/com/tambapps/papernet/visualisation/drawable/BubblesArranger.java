9c519344b7f366bcd6736c17eb31598c307d136b
nelson888 <tambue@hotmail.fr>
2020-02-12 13:07:22 +0100

    fixed overlaped bubbles when shuffling with animation

--- src/main/java/com/tambapps/papernet/visualisation/drawable/BubblesArranger.java
+++ src/main/java/com/tambapps/papernet/visualisation/drawable/BubblesArranger.java
@@ -3,13 +3,11 @@ package com.tambapps.papernet.visualisation.drawable;
 import com.tambapps.papernet.visualisation.animation.Animation;
 import com.tambapps.papernet.visualisation.animation.MoveAnimation;
 import com.tambapps.papernet.visualisation.animation.interpolation.Interpolation;
-import org.joml.Vector2f;
+import org.joml.Vector3f;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Consumer;
 
 import static com.tambapps.papernet.gl.GlWindow.WINDOW_HEIGHT;
@@ -17,62 +15,58 @@ import static com.tambapps.papernet.gl.GlWindow.WINDOW_WIDTH;
 
 public class BubblesArranger {
 
-  private static final Vector2f tempVec = new Vector2f();
+  private static final Vector3f tempVec = new Vector3f();
+
+  private static boolean overlaps(float x1, float y1, float radius1, float x2, float y2, float radius2) {
+    float xDif = x1 - x2;
+    float yDif = y1 - y2;
+    float distanceSquared = xDif * xDif + yDif * yDif;
+    radius1 = radius1 * 2f; // in order to have some space between each bubble
+    return distanceSquared < (radius1 + radius2) * (radius1 + radius2);
+  }
 
   private static boolean overlaps(Bubble b1, Bubble b2) {
-    float xDif = b1.getX() - b2.getX();
-    float yDif = b1.getY() - b2.getY();
-    float distanceSquared = xDif * xDif + yDif * yDif;
-    float radius1 = b1.getRadius() * 2f; // in order to have some space between each bubble
-    float radius2 = b2.getRadius();
-    return distanceSquared < (radius1 + radius2) * (radius1 + radius2);
+    return overlaps(b1.getX(), b1.getY(), b1.getRadius(),
+      b2.getX(), b2.getY(), b2.getRadius());
   }
 
   private static boolean overlaps(Bubble bubble, Collection<Bubble> bubbles) {
     return bubbles.stream()
-      .filter(b -> b != bubble)
       .anyMatch(b -> overlaps(bubble, b));
   }
 
   public static void arrange(Collection<Bubble> bubbles) {
     List<Bubble> arrangedBubbles = new ArrayList<>();
     for (Bubble bubble : bubbles) {
-      boolean placed = false;
-      while (!placed) {
+      while (true) {
         bubble.setX((float)Math.random() * WINDOW_WIDTH - (WINDOW_WIDTH >> 1));
         bubble.setY((float)Math.random() * WINDOW_HEIGHT - (WINDOW_HEIGHT >> 1));
         if (!overlaps(bubble, arrangedBubbles)) {
-          placed = true;
+          arrangedBubbles.add(bubble);
+          break;
         }
-        arrangedBubbles.add(bubble);
       }
     }
   }
-  private static boolean overlaps(Bubble bubble, Vector2f endPosition, Collection<Bubble> bubbles) {
-    float originalX = bubble.getX();
-    float originalY = bubble.getY();
-    try {
-      bubble.setPosition(endPosition.x, endPosition.y);
-      return overlaps(bubble, bubbles);
-    } finally {
-      bubble.setPosition(originalX, originalY);
-    }
+  private static boolean overlaps(Vector3f endPosition, Collection<Vector3f> bubblePositions) {
+    return bubblePositions.stream()
+      .anyMatch(position -> overlaps(endPosition.x, endPosition.y, endPosition.z,
+        position.x, position.y, position.z));
   }
 
-  // TODO BUG, SOME BUBLES OVERLAP EACH OTHERS
   public static void arrangeWithAnimation(Collection<Bubble> bubbles, Consumer<Animation> animationConsumer) {
-    Map<Bubble, Vector2f> bubbleEndPositionMap = new HashMap<>();
+    List<Vector3f> arrangedBubblePositions = new ArrayList<>(); // x, y, radius
     for (Bubble bubble : bubbles) {
-      boolean placed = false;
-      while (!placed) {
-        Vector2f endPosition = tempVec.set(
+      while (true) {
+        Vector3f endPosition = tempVec.set(
           (float)Math.random() * WINDOW_WIDTH - (WINDOW_WIDTH >> 1),
-          (float)Math.random() * WINDOW_HEIGHT - (WINDOW_HEIGHT >> 1)
+          (float)Math.random() * WINDOW_HEIGHT - (WINDOW_HEIGHT >> 1),
+          bubble.getRadius()
         );
-        if (!overlaps(bubble, endPosition, bubbleEndPositionMap.keySet())) {
-          placed = true;
+        if (!overlaps(endPosition, arrangedBubblePositions)) {
           animationConsumer.accept(new MoveAnimation(bubble, endPosition.x, endPosition.y, 1f, Interpolation.linear()));
-          bubbleEndPositionMap.put(bubble, endPosition);
+          arrangedBubblePositions.add(new Vector3f(endPosition));
+          break;
         }
       }
     }
